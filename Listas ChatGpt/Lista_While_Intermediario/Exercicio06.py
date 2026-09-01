e6f2da04b7296752b4a6fda546d69b6db37ea774
vitordeev <victordevxz@gmail.com>
2026-09-01 19:47:01 -0300

    Meia bomba

--- Listas ChatGpt/Lista_While_Intermediario/Exercicio06.py
+++ Listas ChatGpt/Lista_While_Intermediario/Exercicio06.py
@@ -1,5 +1,5 @@
-"""Maior número
-Peça números até receber 0. No final, mostre o maior número informado."""
+"""Menor número
+Peça números até receber 0. No final, mostre o menor número informado."""
 
 num = int(input("Digite um número (ou 0 para encerrar): "))
 soma = 0 
@@ -12,4 +12,4 @@ while num != 0:
 
        num = int(input("Digite um número (ou 0 para encerrar): "))
        
-print("Programa encerrado. O maior número informado foi:", soma)
+print("Programa encerrado. O menor número informado foi:", soma)
