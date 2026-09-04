737b9cb8674eaf9f357e7f2b50e194daa8d61735
vitordeev <victordevxz@gmail.com>
2026-09-04 07:32:12 -0300

    Primeiro do dia

--- Programming and Algorithms/Lista 04/Aul.py
+++ Programming and Algorithms/Lista 04/Aul.py
@@ -5,8 +5,10 @@ while cont != 10:
     cont += 1
     print(f"{cont} X {num} = {cont* num}")
 
-
+cont = 1
 senha = input("Sua senha e python123: ")
 while senha != ("python123"):
+    cont += 1
     senha = input("Sua senha e python123:")
+    print(f"Voce tentou {cont} vezes")
 print("Programa Encerrado")
